abd580d1111b40eeca4a6629bcb5bba3f80f02e0
magaly68 <wintzer.magaly@gmail.com>
2026-06-12 19:21:46 +0200

    CONFIGURATION PWA

--- epiguard-landing/vite.config.js
+++ epiguard-landing/vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'EpiGuard',
+        short_name: 'EpiGuard',
+        description:
+          'Site de présentation et de recrutement de testeurs pour l’application EpiGuard.',
+        theme_color: '#2563eb',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
